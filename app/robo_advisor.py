da07b5c48355e72aa097390c84d1adcf7b8abed6
tatsukiKikugawa <tk802@georgetown.edu>
2020-02-27 15:19:37 -0500

    Get latest refreshed data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import os
+import csv
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,25 +12,51 @@ print("REQUESTING SOME DATA FROM THE INTERNET...")
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
 
-symbol = "TSLA" #to do; ask for a user input
+#Processing  user inputs.
+symbol = input("Please type a stock symbol you are looking for:  ")
+print(f"You chose: " + str(symbol))
+
+#Vadlidating user inputs
+if len(symbol) != 4:
+    print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+    exit()
+#elif int(symbol) == [0,99999]:
+#    print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+#    exit()
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}]"
 
-print("URL: ", request_url)
-
+#print("URL: ", request_url)
 response = requests.get(request_url)
-print(type(response))
-print(dir(response))
+#print(type(response))   #>'requests.models.Response'
+#print(dir(response))
+#print(response.status_code)
+#print(response.text)
+#print(type(response.text))  #>'str'
 
-print(response.status_code)
-print(response.text)
-print(type(response.text))
+#handle response errors
+if "Error Message" in response.text:
+    print("OOPS couldn't find that symbol, please try again!")
+    exit()
 
-parsed_response = json.loads(response.text)
-print(type(parsed_response))    #> list
+parsed_response = json.loads(response.text) #>to parse response.text (str) to dict
+#print(type(parsed_response))    
+#> list dict_keys(['Meta Data', 'Time Series (Daily)'])
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+#Writing a CSV file 
+csv_file_path = "data/stock.csv" 
+
+
+
+tsd = parsed_response["Time Series (Daily)"]
+for date, prices in tsd.items():
+    print(date)
+    print(prices)
+    print("----")
 #print(parsed_response)
-#breakpoint()
+
+
 #
 #first_prod = parsed_response[0]
 #print(first_prod["name"])
@@ -40,7 +67,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
